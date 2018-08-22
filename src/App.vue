52ce74bc19357a060e770ca0e48cc582367583b1
dongatuna <ngatuna05@gmail.com>
2018-08-21 19:32:58 -0700

    first commit of vue project

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <pp-header></pp-header>
+    </div>
+    <div class="container">
+      <pp-menu></pp-menu>
+    </div>
+    <div class="container">
+      <pp-footer></pp-footer>
+    </div>
+   
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+import Home from "./components/Home.vue";
+import Menu from "./components/Menu.vue";
+
+export default {
+  //register the above components
+  components:{
+    "pp-header":Header,
+    "pp-footer":Footer,
+    "pp-home":Home,
+    "pp-menu":Menu
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
